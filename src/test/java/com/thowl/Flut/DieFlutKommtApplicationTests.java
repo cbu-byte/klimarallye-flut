779f138971e2cc789f6299f90e78c5c243ab3cc7
cbu-byte <cedric9922@gmail.com>
2024-05-01 10:56:48 +0200

    first commit

---
+++ src/test/java/com/thowl/Flut/DieFlutKommtApplicationTests.java
@@ -0,0 +1,13 @@
+package com.thowl.Flut;
+
+import org.junit.jupiter.api.Test;
+import org.springframework.boot.test.context.SpringBootTest;
+
+@SpringBootTest
+class DieFlutKommtApplicationTests {
+
+	@Test
+	void contextLoads() {
+	}
+
+}
