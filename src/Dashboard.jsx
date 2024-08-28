56df608d0472a285ce1ffe9e75ddb3fd90816abf
cedric <cedric9922@gmail.com>
2024-08-28 15:18:36 +0200

    jwt decode fixed

--- src/Dashboard.jsx
+++ src/Dashboard.jsx
@@ -1,22 +1,53 @@
-// Dashboard.jsx
-import React, { useEffect } from 'react';
-
-
-
-const levels = [
-  { id: 1, name: 'Level 1', map: 'MapImage1.jpg', initialWaterLevel: 1, maxWaterLevel: 1.2 },
-  { id: 2, name: 'Level 2', map: 'Map2.jpg', initialWaterLevel: 1.5, maxWaterLevel: 2.5 },
-  { id: 3, name: 'Level 3', map: 'Map3.jpg', initialWaterLevel: 2, maxWaterLevel: 4 },
-];
-useEffect(() => {
-  const jwtToken = getCookie("jwt");
-
-   if(jwtToken){
-   setToken(jwtToken);
-}
-}, []);
+import React, { useEffect, useState} from 'react';
+import { jwtDecode } from "jwt-decode";
+import { getCookie } from "./lib/cookieUtils";
 
 function Dashboard({ onSelectLevel }) {
+  //const [userId, setUserId] = useState(null); // for User Identification
+  const levels = [
+    { id: 1, name: 'Level 1', map: 'MapImage1.jpg', initialWaterLevel: 1, maxWaterLevel: 1.2 },
+    { id: 2, name: 'Level 2', map: 'Map2.jpg', initialWaterLevel: 1.5, maxWaterLevel: 2.5 },
+    { id: 3, name: 'Level 3', map: 'Map3.jpg', initialWaterLevel: 2, maxWaterLevel: 4 },
+  ];
+  
+/*
+  useEffect(() => {
+    const jwtToken = getCookie("jwt");
+  
+    if (jwtToken) {
+      console.log("JWT Token gefunden:", jwtToken); // Debug-Ausgabe
+      try {
+        const decodedToken = jwtDecode(jwtToken);
+        console.log("Decoded Token:", decodedToken); // Debug-Ausgabe
+        const userIdFromToken = decodedToken.sub;
+        setUserId(userIdFromToken);
+      } catch (error) {
+        console.error('Fehler beim Decodieren des JWT-Tokens:', error);
+      }
+    }
+  }, []);
+  */
+  useEffect(() => {
+    const jwtToken = getCookie("jwt");
+
+    if (jwtToken) {
+      setToken(jwtToken);
+      
+      try {
+        // Decodieren des Tokens, um die Payload zu extrahieren
+        const decodedToken = jwtDecode(jwtToken);
+        // Extrahiere die user_id aus der Payload
+        const userIdFromToken = decodedToken.sub; // Verwende hier "sub", da du die user_id unter "sub" gesetzt hast
+
+        // Setze die user_id in den State
+        //setUserId(userIdFromToken);
+      } catch (error) {
+        console.error('Fehler beim Decodieren des JWT-Tokens:', error);
+      }
+        
+    }
+  }, []);
+
   return (
     <div className="min-h-screen flex flex-col items-center justify-top">
       <h1 className="text-3xl mb-4">Wähle ein Level</h1>
@@ -28,7 +59,7 @@ function Dashboard({ onSelectLevel }) {
               className="btn btn-primary"
               onClick={() => onSelectLevel(level)} // Klick-Event für die Levelauswahl
             >
-               {level.name} {/* Name des Levels */}
+              {level.name} {/* Name des Levels */}
             </button>
           </li>
         ))}
@@ -38,3 +69,4 @@ function Dashboard({ onSelectLevel }) {
 }
 
 export default Dashboard;
+
